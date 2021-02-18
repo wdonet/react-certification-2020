ed120771da4857c8e437e3d0c387a6811720fd3e
saulmeneses <saul.meneses@wizeline.com>
2021-02-18 10:19:18 -0700

    Created Header component

---
+++ src/components/Header/Header.component.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import { Toolbar, IconButton, FormControlLabel, Menu, MenuItem } from '@material-ui/core';
+import {
+  Menu as MenuIcon,
+  Search as SearchIcon,
+  AccountCircle as AccountCircleIcon,
+} from '@material-ui/icons';
+
+import {
+  StyledGrowDiv,
+  StyledAppBar,
+  StyledIconButton,
+  StyledSearchDiv,
+  SearchIconDiv,
+  StyledInputBase,
+  StyledInput,
+  StyledDarkModeDiv,
+  StyledSwitch,
+} from './styled';
+
+const Header = () => {
+  const [checked, setChecked] = useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const isMenuOpen = Boolean(anchorEl);
+
+  const handleMenuOpen = (e) => setAnchorEl(e.currentTarget);
+
+  const handleMenuClose = () => setAnchorEl(null);
+
+  return (
+    <StyledGrowDiv height>
+      <StyledAppBar position="static">
+        <Toolbar>
+          <StyledIconButton edge="start" color="inherit" aria-label="open drawer">
+            <MenuIcon />
+          </StyledIconButton>
+
+          <StyledSearchDiv>
+            <SearchIconDiv>
+              <SearchIcon />
+            </SearchIconDiv>
+
+            <StyledInputBase
+              placeholder="Search…"
+              inputComponent={StyledInput}
+              inputProps={{ 'aria-label': 'search' }}
+            />
+          </StyledSearchDiv>
+
+          <StyledGrowDiv />
+
+          <StyledDarkModeDiv>
+            <FormControlLabel
+              label="Dark mode"
+              control={
+                <StyledSwitch
+                  checked={checked}
+                  onChange={(e) => setChecked(e.target.checked)}
+                  aria-label="dark mode switch"
+                />
+              }
+            />
+            <IconButton
+              edge="end"
+              aria-label="account of current user"
+              aria-controls="menuId"
+              aria-haspopup="true"
+              onClick={handleMenuOpen}
+              color="inherit"
+            >
+              <AccountCircleIcon />
+            </IconButton>
+            <Menu
+              id="menuId"
+              anchorEl={anchorEl}
+              open={isMenuOpen}
+              onClose={handleMenuClose}
+              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              keepMounted
+            >
+              <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+              <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+            </Menu>
+          </StyledDarkModeDiv>
+        </Toolbar>
+      </StyledAppBar>
+    </StyledGrowDiv>
+  );
+};
+
+export default Header;
---
+++ src/components/Header/styled.jsx
@@ -0,0 +1,91 @@
+import styled, { css } from 'styled-components';
+import { AppBar, IconButton, InputBase, Switch } from '@material-ui/core';
+
+export const StyledGrowDiv = styled.div`
+  flex-grow: 1;
+
+  ${(props) =>
+    props.height &&
+    css`
+      height: 100%;
+    `};
+`;
+
+export const StyledAppBar = styled(AppBar)`
+  && {
+    background-color: #1c5476;
+  }
+`;
+
+export const StyledIconButton = styled(IconButton)`
+  margin-right: 16px;
+`;
+
+export const StyledSearchDiv = styled.div`
+  width: 100%;
+  position: relative;
+  border-radius: 4px;
+  margin-right: 16px;
+  margin-left: 0;
+  background-color: #ffffff15;
+
+  &:hover {
+    background-color: #ffffff25;
+  }
+
+  @media (min-width: 600px) {
+    margin-left: 24px;
+    width: auto;
+  }
+`;
+
+export const SearchIconDiv = styled.div`
+  position: absolute;
+  height: 100%;
+  padding: 0px 16px;
+  pointer-events: none;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const StyledInputBase = styled(InputBase)`
+  color: inherit;
+`;
+
+export const StyledInput = styled.input`
+  && {
+    width: 100%;
+    padding: 8px 8px 8px 0px;
+    padding-left: calc(1em + 32px);
+    color: #ffff;
+
+    @media (min-width: 960px) {
+      width: 20ch;
+    }
+  }
+`;
+
+export const StyledDarkModeDiv = styled.div`
+  display: none;
+
+  @media (min-width: 960px) {
+    display: flex;
+  }
+`;
+
+export const StyledSwitch = styled(Switch)`
+  ${(props) =>
+    props.checked &&
+    css`
+      && {
+        .Mui-checked {
+          color: #19857b;
+        }
+        .Mui-checked + .MuiSwitch-track {
+          background-color: #19857b;
+        }
+      }
+    `};
+`;
