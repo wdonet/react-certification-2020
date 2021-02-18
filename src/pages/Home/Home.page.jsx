384ea0c33dd36357f4b199258cf336214771fc0d
Juan Hernandez <juan.rangel@wizeline.com>
2021-02-18 17:55:14 -0600

    feat: pure css, and basic html tags added to home page
    mock videos file implemented and rendered

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,8 +1,8 @@
 import React, { useRef } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-
 import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
+import videos from '../../providers/mock/youtube-videos-mock.json';
 
 function HomePage() {
   const history = useHistory();
@@ -17,7 +17,42 @@ function HomePage() {
 
   return (
     <section className="homepage" ref={sectionRef}>
-      <h1>Hello stranger!</h1>
+      <div className="sidebar toggle-sidebar">
+        <ul>
+          <li>Home</li>
+          <li>Favorites</li>
+        </ul>
+      </div>
+      <div className="main">
+        <div className="header-panel">
+          <div className="ui icon button toggle-button">
+            <i className="bars icon"></i>
+          </div>
+          <input type="text" className="search-input" placeholder="Search..."/>
+          <div className="control-panel">
+            <div className="ui toggle checkbox">
+              <input type="checkbox" />
+              <label>Dark mode</label>
+            </div>
+            <div className="ui red button">
+              <i className="sign in alternate icon"></i>
+              Sign In
+            </div>
+          </div>
+        </div>
+        <div className="content-wrapper">
+          { videos.items.map(video => {
+            return (
+              <div className="ui card">
+                <div className="content">
+                  <div className="header">{video.snippet.channelTitle}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      {/* <h1>Hello stranger!</h1>
       {authenticated ? (
         <>
           <h2>Good to have you back</h2>
@@ -31,7 +66,7 @@ function HomePage() {
         </>
       ) : (
         <Link to="/login">let me in →</Link>
-      )}
+      )} */}
     </section>
   );
 }
