cf49d30a4213c568d33fba8c56be4910c3c77792
Luis Venegas <luis.venegas@wizeline.com>
2021-02-19 17:05:34 -0600

    - Remove Unused components

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
 import NotFound from '../../pages/NotFound';
 import Layout from '../Layout';
@@ -12,18 +11,16 @@ import '../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min';
 function App() {
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Layout>
-          <Switch>
-            <Route exact path="/">
-              <HomePage />
-            </Route>
-            <Route path="*">
-              <NotFound />
-            </Route>
-          </Switch>
-        </Layout>
-      </AuthProvider>
+      <Layout>
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
     </BrowserRouter>
   );
 }
