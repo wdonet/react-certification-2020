9416c0f9980ab7fb0697a644afc44df4e59efca2
Abraham Ortiz <Abraham.Ortiz@dowjones.com>
2021-02-20 00:11:42 -0600

    fix: remove useLayoutEffect from app

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from 'react';
+import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
