d964e49a8f1d298d4095cd4d2b7ab93d687f31ed
Luis Venegas <luis.venegas@wizeline.com>
2021-02-19 19:50:59 -0600

    - Remove unused method

--- src/utils/htmlEntities.js
+++ src/utils/htmlEntities.js
@@ -1,19 +1,5 @@
 export const htmlentities = {
   /**
-   * Converts a string to its html characters completely.
-   *
-   * @param {String} str String with unescaped HTML characters
-   * */
-  encode(str) {
-    const buf = [];
-
-    for (let i = str.length - 1; i >= 0; i -= 1) {
-      buf.unshift(['&#', str[i].charCodeAt(), ';'].join(''));
-    }
-
-    return buf.join('');
-  },
-  /**
    * Converts an html characterSet into its original character.
    *
    * @param {String} str htmlSet entities
