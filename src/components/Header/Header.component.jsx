cef0eea0380476d3e3970308211bfb06f1457109
Orlando Rincón <orlando.rincon@wizeline.com>
2021-02-22 00:46:44 -0600

    Clean errors

--- src/components/Header/Header.component.jsx
+++ src/components/Header/Header.component.jsx
@@ -11,7 +11,7 @@ const Header = () => (
     <Styled.Search placeholder="Type something cool" />
     <Styled.RightHolder>
       <Styled.ToggleButton id="darkMode" type="checkbox" />
-      <label htmlFor="darkMode">Dark Mode</label>
+      <span>Dark Mode</span>
       <Styled.ProfileButton
         type="image"
         src="https://img2.freepng.es/20180422/wee/kisspng-computer-icons-user-profile-login-clip-art-my-account-icon-5adc5dd8d9ca10.9425519815243913848921.jpg"
--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,35 +1,16 @@
-import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-
-import { useAuth } from '../../providers/Auth';
+import React from 'react';
 import mockedData from '../../utils/mocked-youtube.json';
 import VideoList from '../../components/VideoList';
 
 function HomePage() {
-  const history = useHistory();
-  const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
-
   const { items } = mockedData;
 
   return (
-    <section ref={sectionRef}>
-      {authenticated || true ? (
-        <VideoList
-          title="Welcome to Orlando's YouTube Client"
-          items={items}
-          filter="videos"
-        />
-      ) : (
-        <Link to="/login">let me in →</Link>
-      )}
-    </section>
+    <VideoList
+      title="Welcome to Orlando's YouTube Client"
+      items={items}
+      filter="videos"
+    />
   );
 }
 
