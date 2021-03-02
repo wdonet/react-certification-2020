3520a8428b46c2741cc36eb61bc39c4ede2b4c8f
saulmeneses <saul.meneses@wizeline.com>
2021-03-02 12:15:49 -0700

    Added test for Login component

---
+++ src/pages/Login/__tests__/Login.spec.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { render, screen, createEvent, fireEvent } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+
+import Login from '..';
+import AuthProvider, { AuthContext } from '../../../providers/Auth';
+
+describe('<Login />', () => {
+  it('renders login form', () => {
+    render(
+      <AuthProvider>
+        <Login />
+      </AuthProvider>
+    );
+
+    expect(screen.getByLabelText('username')).toBeInTheDocument();
+    expect(screen.getByLabelText('password')).toBeInTheDocument();
+  });
+
+  it('executes authentication on submit', () => {
+    const loginMock = jest.fn();
+    const historyMock = { push: jest.fn(), listen: jest.fn(), location: {} };
+
+    render(
+      <Router history={historyMock}>
+        <AuthContext.Provider value={{ login: loginMock }}>
+          <Login />
+        </AuthContext.Provider>
+      </Router>
+    );
+
+    const element = screen.getByText('login');
+
+    const clickEvent = createEvent.click(element);
+
+    fireEvent.click(element, clickEvent);
+    expect(loginMock).toHaveBeenCalled();
+    expect(historyMock.push).toHaveBeenCalled();
+  });
+});
