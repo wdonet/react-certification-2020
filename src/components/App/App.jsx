cylcrow/react-certification-2021

:recycle: :art: :sparkles: :truck: Adding youtube playback iframe
- Added test for YT playback frame
- Added component for youtube playback

src/components/App/App.jsx
@@ -3,6 +3,7 @@ import styled, { ThemeProvider } from 'styled-components';
 import { LayoutWrapper } from '../Layout';
 import HomeVideos from '../HomeVideos/HomeVideos';
 import { lightTheme, darkTheme } from '../../providers/themes';
+import VideoPlayerContainer from '../HomeVideos/VideoPlayerContainer';
 
 const StyledWelcome = styled.div`
   text-align: center;
@@ -10,6 +11,7 @@ const StyledWelcome = styled.div`
 
 function App() {
   const [isLightTheme, setIsLightTheme] = useState(true);
+  const [videoID, setVideoID] = useState(null);
 
   const switchTheme = () => {
     setIsLightTheme(!isLightTheme);
@@ -23,10 +25,18 @@ function App() {
   return (
     <ThemeProvider theme={getThemeConfig()}>
       <LayoutWrapper>
-        <StyledWelcome>
-          <h1>Welcome to the challenge!</h1>
-        </StyledWelcome>
-        <HomeVideos />
+        {videoID ? (
+          <div data-testid="video-player-container">
+            <VideoPlayerContainer />
+          </div>
+        ) : (
+          <div>
+            <StyledWelcome>
+              <h1>Welcome to the challenge!</h1>
+            </StyledWelcome>
+            <HomeVideos playVideoNow={setVideoID} />
+          </div>
+        )}
       </LayoutWrapper>
     </ThemeProvider>
   );

wdonet/react-certification-2020

src/components/App/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import styled, { ThemeProvider } from 'styled-components';
 import { LayoutWrapper } from '../Layout';
-import HomeVideos from '../HomeVideos/HomeVideos';
 import { lightTheme, darkTheme } from '../../providers/themes';
+import VideoPlayerContainer from '../VideoPlayer/VideoPlayerContainer';
+import HomeVideos from '../HomeVideos/HomeVideos';
 
 const StyledWelcome = styled.div`
   text-align: center;
@@ -10,6 +11,7 @@ const StyledWelcome = styled.div`
 
 function App() {
   const [isLightTheme, setIsLightTheme] = useState(true);
+  const [videoID, setVideoID] = useState(null);
 
   const switchTheme = () => {
     setIsLightTheme(!isLightTheme);
@@ -23,10 +25,18 @@ function App() {
   return (
     <ThemeProvider theme={getThemeConfig()}>
       <LayoutWrapper>
-        <StyledWelcome>
-          <h1>Welcome to the challenge!</h1>
-        </StyledWelcome>
-        <HomeVideos />
+        {videoID ? (
+          <div data-testid="video-player-container">
+            <VideoPlayerContainer videoId={videoID}/>
+          </div>
+        ) : (
+          <div>
+            <StyledWelcome>
+              <h1>Welcome to the challenge!</h1>
+            </StyledWelcome>
+            <HomeVideos playVideoNow={setVideoID} />
+          </div>
+        )}
       </LayoutWrapper>
     </ThemeProvider>
   );