45644b76d218c277c5dc6a18096b305df8679742
Andrés Billini <ext-a.billini@rappi.com>
2021-03-23 18:33:22 -0500

    Update App.component.jsx

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, HashRouter } from 'react-router-dom';
 
 import AuthProvider from '../../providers/Auth';
 import HomePage from '../../pages/Home';
@@ -13,7 +13,7 @@ import VideoDetails from '../../pages/VideoDetails/VideoDetails';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AuthProvider>
         <ThemeBaseProvider>
           <Layout>
@@ -37,7 +37,7 @@ function App() {
           </Layout>
         </ThemeBaseProvider>
       </AuthProvider>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
